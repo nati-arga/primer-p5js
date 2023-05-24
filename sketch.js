9b6a2da5b985ebe11885064ff9a20409cc4cb18f
nati-arga <nataliadaniela.88@gmail.com>
2023-05-24 02:53:43 -0300

    Creamos el primer commit del proyecto

---
+++ sketch.js
@@ -0,0 +1,68 @@
+/*
+Composición, 1952. -José Pedro Costigliolo-
+V1
+Natalia Argañaraz
+*/
+
+function setup() {
+    createCanvas(500, 700);
+    background(90, 0, 255);
+  
+    noStroke();
+    //inicio fondo celeste:
+    fill(100, 175, 250);
+    rect(0, 0, 500, 700);
+    //fin fondo celeste
+  
+    //inicio fondo amarillo:
+    fill(255, 175, 0);
+    rect(0, 480, 60, 220);
+    rect(60, 530, 290, 250);
+    rect(350, 450, 40, 250);
+    rect(390, 200, 500, 500);
+    //fin fondo amarillo
+  
+    //inicio figuras de fondo
+    //rect amarillo
+    rect(200, 150, 150, 100);
+    //rect marrón grande:
+    fill(128, 64, 50);
+    rect(200, 250, 150, 200);
+    //circulo blanco
+    fill(255);
+    circle(150, 475, 220);
+    //rect marrón pequeño:
+    fill(128, 64, 50);
+    rect(100, 450, 100, 30);
+    //cuadrado blanco:
+    fill(255);
+    rect(50, 100, 150, 150);
+    //rect blanco vertical
+    rect(350, 200, 40, 250);
+    //fin figuras de fondo*/
+  
+    //inicio figuras horizontales negras
+    fill(0);
+    rect(125, 50, 280, 20);
+    rect(50, 100, 150, 20);
+    rect(170, 140, 180, 10);
+    rect(125, 180, 380, 20);
+    rect(50, 250, 400, 10);
+    rect(100, 440, 290, 10);
+    rect(0, 480, 200, 10);
+    rect(170, 530, 180, 20);
+    rect(100, 620, 360, 20);
+    //fin figuras horizontales negras
+  
+    //inicio verticales horizontales negras
+    rect(40, 100, 10, 160);
+    rect(100, 440, 20, 200);
+    rect(110, 50, 20, 150);
+    rect(170, 150, 10, 390);
+    rect(200, 100, 10, 390);
+    rect(340, 140, 20, 410);
+    rect(390, 50, 20, 400);
+    rect(440, 250, 20, 390);
+    //fin verticales horizontales negras
+  }
+  
